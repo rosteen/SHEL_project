9b52a36c36fd309329a01cada046fc3f4704b88e
Ricky O'Steen <rosteen@stsci.edu>
2022-04-13 14:11:35 -0400

    Fixing bugs
    More bug fixes, forgot to generalize one part
    
    Bugfix
    
    Better fix
    
    Put fix in the actual right place...

--- fitter.py
+++ fitter.py
@@ -45,7 +45,7 @@ class SHEL_Fitter():
         lc_insts = self.cur.execute(stmt).fetchall()
 
         # Concat ref_id with instrument name for
-        lc_inst_names = [f"{x[1]}-{x[0]}" for x in rv_insts]
+        lc_inst_names = [f"{x[1]}-{x[0]}".replace("-None", "") for x in lc_insts]
         return lc_inst_names
 
     def get_light_curve_data(self, TESS_only=False):
@@ -87,7 +87,7 @@ class SHEL_Fitter():
             fluxes[lc_inst_name] = flux 
             fluxes_error[lc_inst_name] = flux_error
 
-        return times_rv, fluxes, fluxes_error
+        return times, fluxes, fluxes_error
 
     def get_rv_data(self, rv_inst_names=None):
         times_rv, data_rv, errors_rv = {}, {}, {}
@@ -251,8 +251,8 @@ class SHEL_Fitter():
             hyperps += [[a, a_err],]
         else:
             # Retrieve Rho parameter from database
-            stmt = "select rho, rho_err from stellar_parameters where "
-                   f"target_id = {self.target_id}"
+            stmt = ("select rho, rho_err from stellar_parameters where "
+                    f"target_id = {self.target_id}")
             rho, rho_err = self.cur.execute(stmt).fetchone()
             params += ['rho',]
             dists += ['TruncatedNormal',]
@@ -313,13 +313,14 @@ class SHEL_Fitter():
                 self.oot_phase_limit = duration/(period*24)
 
             # Get phases and sort data
-            phases = juliet.get_phases(times['TESS'], period, t0)
-            idx_oot = np.where(np.abs(phases)<=self.oot_phase_limit)[0]
-            sort_times = np.argsort(times['TESS'][idx_oot])
+            for inst in times:
+                phases = juliet.get_phases(times[inst], period, t0)
+                idx_oot = np.where(np.abs(phases)<=self.oot_phase_limit)[0]
+                sort_times = np.argsort(times[inst][idx_oot])
 
-            times['TESS'] = times['TESS'][idx_oot][sort_times]
-            fluxes['TESS'] = fluxes['TESS'][idx_oot][sort_times]
-            fluxes_error['TESS'] = fluxes_error['TESS'][idx_oot][sort_times]
+                times[inst] = times[inst][idx_oot][sort_times]
+                fluxes[inst] = fluxes[inst][idx_oot][sort_times]
+                fluxes_error[inst] = fluxes_error[inst][idx_oot][sort_times]
 
         out_folder = f"juliet_fits/{self.target}"
         kwargs = {"priors": self.priors, "t_lc": times, "y_lc": fluxes,
